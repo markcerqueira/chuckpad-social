da0172a304261120a5d970a97e035d83a7a83d17
Mark Cerqueira <mark.cerqueira@gmail.com>
2016-11-13 23:36:15 -0800

    Check for SendGrid API key before trying to send email

--- controllers/application_controller.rb
+++ controllers/application_controller.rb
@@ -19,6 +19,12 @@ module MailHelper
 
   # Helper method to send someone an email
   def self.send_email(to_field, subject_text, html_body_text)
+    sendgrid_api_key = ENV['SENDGRID_API_KEY']
+    if sendgrid_api_key.nil? or sendgrid_api_key.empty?
+      puts 'send_email - SendGrid API key is not configured so cannot send email'
+      return
+    end
+
     begin
       from = Email.new(email: ENV['EMAIL_FROM_ADDRESS'].to_s, name: ENV['EMAIL_FROM_NAME'].to_s)
       to = Email.new(email: to_field)
